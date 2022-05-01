e87a495b77a881e449252542c8f0a93329da65dc
kaiyamag <kmagnuson24@wooster.edu>
2022-05-01 12:34:10 -0400

    Add SQL statements to create books, library_members, classes, and class_members tables

--- app/utils/db.py
+++ app/utils/db.py
@@ -29,13 +29,56 @@ def init_db(config):
     cursor.execute(f"use {config['DATABASE']};")
     cursor.execute(
         f""" 
-        CREATE TABLE tasks
+        CREATE TABLE library_members
         (
-            id SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
-            description VARCHAR(50),
-            creation_datetime timestamp,
-            completed TINYINT(1),
-            CONSTRAINT pk_todo PRIMARY KEY (id)
+            account_id SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
+            fname VARCHAR(50),
+            lname VARCHAR(50),
+            account_type ENUM('STUDENT', 'PROFESSOR', 'STAFF'),
+            CONSTRAINT pk_library_members PRIMARY KEY (account_id)
+        );
+        """
+    )
+    cursor.execute(
+        f""" 
+        CREATE TABLE books
+        (
+            book_id SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
+            title VARCHAR(50),
+            author_fname VARCHAR(50),
+            author_lname VARCHAR(50),
+            publication_year YEAR,
+            checked_out_to SMALLINT UNSIGNED,
+            CONSTRAINT fk_check_out FOREIGN KEY (checked_out_to)
+            REFERENCES library_members (account_id),
+            CONSTRAINT pk_books PRIMARY KEY (book_id)
+        );
+        """
+    )
+    cursor.execute(
+        f""" 
+        CREATE TABLE classes
+        (
+            class_id SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
+            course_title VARCHAR(50),
+            reference_book SMALLINT UNSIGNED,
+            CONSTRAINT fk_reference_book FOREIGN KEY (reference_book)
+            REFERENCES books (book_id),
+            CONSTRAINT pk_classes PRIMARY KEY (class_id)
+        );
+        """
+    )
+    cursor.execute(
+        f""" 
+        CREATE TABLE class_members
+        (
+            account_id SMALLINT UNSIGNED,
+            class_id SMALLINT UNSIGNED,
+            CONSTRAINT fk_account_ID FOREIGN KEY (account_id)
+            REFERENCES library_members (account_id),
+            CONSTRAINT fk_class_ID FOREIGN KEY (class_id)
+            REFERENCES classes (class_id),
+            CONSTRAINT pk_class_members PRIMARY KEY (account_id, class_id)
         );
         """
     )
