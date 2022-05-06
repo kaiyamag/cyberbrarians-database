1aa17472cabbcad2dc40996f84616216c0399a15
kaiyamag <kmagnuson24@wooster.edu>
2022-05-06 11:44:13 -0400

    Add new route to show a list of all patrons, instead of printing this list in the index

--- app/views/patron_view.py
+++ app/views/patron_view.py
@@ -4,20 +4,29 @@ from api.patron_api import Patron, PatronDB
 
 patron_table_blueprint = Blueprint('patron_table_blueprint', __name__)
 
-@patron_table_blueprint.route('/', methods=["GET", "POST"])
-def index():
-    database = PatronDB(g.mysql_db, g.mysql_cursor)
+# REMOVED PATRON INDEX ROUTE
 
-    if request.method == "POST":
-        patron_ids = request.form.getlist("account_id")
-        for patron_id in patron_ids:
-            database.delete_patron_by_id(patron_id)
+# @patron_table_blueprint.route('/', methods=["GET", "POST"])
+# def index():
+#     database = PatronDB(g.mysql_db, g.mysql_cursor)
 
-    return render_template('index.html', patron_table=database.select_all_patrons())    
+#     if request.method == "POST":
+#         # DEBUG
+#         print("Got a POST request")
+
+#         patron_ids = request.form.getlist("account_id")
+#         for patron_id in patron_ids:
+#             database.delete_patron_by_id(patron_id)
+
+#     # DEBUG
+#     print(database.select_all_patrons())
+
+#     return render_template('index.html', patron_table=database.select_all_patrons())    
 
 
 @patron_table_blueprint.route('/patron-entry')
 def patron_entry():
+
    return render_template("patron-entry.html")
 
 
@@ -33,3 +42,18 @@ def add_patron():
     database.insert_patron(new_patron)
 
     return redirect('/')
+
+@patron_table_blueprint.route('/patron-list', methods=["GET", "POST"])
+def patron_list():
+    database = PatronDB(g.mysql_db, g.mysql_cursor)
+
+    if request.method == "POST":
+        # DEBUG
+        print("Got a POST request")
+
+        patron_ids = request.form.getlist("account_id")
+        for patron_id in patron_ids:
+            database.delete_patron_by_id(patron_id)
+
+
+    return render_template('patron-list.html', patron_table=database.select_all_patrons())   
